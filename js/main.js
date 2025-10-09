39f24980bc29733ae5a4bb35589cb8138ed58a0e
javierbull <javierbullreyes@gmail.com>
2025-10-09 17:37:18 -0500

    add control tstimonial carousel

--- js/main.js
+++ js/main.js
@@ -24,27 +24,43 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
+  //   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
 // Owl carousel
 
 $(document).ready(function () {
 
-    $("#owl-testimonials").owlCarousel({
-        autoPlay: true,
-        items: 2,
-        itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
-        itemsMobile : [479,2],
-        pagination: true
-    });
+  var owl = $("#owl-testimonials");
+
+  owl.owlCarousel({
+
+    items: 2, //10 items above 1000px browser width
+    loop: true, // Para un bucle infinito
+    margin: 10, // Margen entre elementos
+    autoplay: true, // Para que se mueva automáticamente
+    nav: true, // Habilita los controles de navegación (flechas)
+    itemsDesktop: [1000, 5], //5 items between 1000px and 901px
+    itemsDesktopSmall: [900, 3], // 3 items betweem 900px and 601px
+    itemsTablet: [600, 2], //2 items between 600 and 0;
+    itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+
+  });
+
+  // Custom Navigation Events
+  $(".next").click(function () {
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function () {
+    owl.trigger('owl.prev');
+  })
+
 
 });
